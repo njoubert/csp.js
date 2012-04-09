e2a3b748a51b0549dcf5d365b1864135b0cd082e
Niels Joubert <njoubert@gmail.com>
2012-04-09 14:07:12 -0700

    initial import

---
+++ src/csp.js
@@ -0,0 +1,20 @@
+/* 
+ * CSP.JS
+ *
+ * A constraint satisfaction problem solver in Javascript.
+ *
+ *
+ */
+ 
+define(function () {
+  
+  
+  
+  
+  return {
+    
+    version: "0.1";
+    
+  }
+  
+});
